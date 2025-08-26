83440bd1701cde3ba9b68acc658e630efcf9d55c
Tom Ellis <tellishtc@gmail.com>
2025-08-26 10:15:17 +0100

    Override default filesytem

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,7 +4,15 @@ namespace SmashedEgg\LaravelRouteAnnotation\Tests;
 
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
+use Illuminate\Contracts\Config\Repository;
+
 class TestCase extends BaseTestCase
 {
-
+     protected function defineEnvironment($app)
+     {
+         // Setup default database to use sqlite :memory:
+         tap($app['config'], function (Repository $config) {
+             $config->set('filesystems.default', 'public');
+         });
+     }
 }
